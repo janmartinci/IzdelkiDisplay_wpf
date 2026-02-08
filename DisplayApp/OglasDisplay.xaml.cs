2fc3e0e5ae86fd47fa9996c76227dc0b7fb78bf0
JM <JM@DESKTOP-F05DH4Q>
2026-02-08 21:06:45 +0100

    OglasDisplay timer and artikel loop

--- DisplayApp/OglasDisplay.xaml.cs
+++ DisplayApp/OglasDisplay.xaml.cs
@@ -1,16 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.DirectoryServices;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Security.Cryptography.Pkcs;
 using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Threading.Tasks;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Automation.Peers;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Xml;
+using System.Xml.Linq;
+using HtmlAgilityPack;
+using MaterialDesignThemes.Wpf;
+using SQLitePCL;
 
 namespace DisplayApp
 {
@@ -20,11 +41,49 @@ namespace DisplayApp
     public partial class OglasDisplay : Window
     {
         private bool FullScreen = false;
+        
+        int CurrentIndex = 0;
+        DispatcherTimer timer = new DispatcherTimer();
+        List<IzdelekModel> ArtikliList = new List<IzdelekModel>();
         public OglasDisplay(string fileName)
         {
             InitializeComponent();
+            if (File.Exists(fileName))
+            {
+                var jsonString = File.ReadAllText($"{fileName}");
+                List<IzdelekModel> deserializedArtikliList = JsonSerializer.Deserialize<List<IzdelekModel>>(jsonString);
+                if (deserializedArtikliList != null)
+                {
+                    ArtikliList = deserializedArtikliList;
+                    StartRotation();
+                }
+            }
+        }
 
-            textOglasTest.Text = fileName;
+        void StartRotation()
+        {
+            timer.Interval = TimeSpan.FromSeconds(30);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            ShowArtikel(ArtikliList[CurrentIndex]);
+        }
+
+         void Timer_Tick(object sender, EventArgs e)
+        {
+            CurrentIndex++;
+
+            if(CurrentIndex >= ArtikliList.Count)
+            {
+                CurrentIndex = 0;
+            }
+
+            ShowArtikel(ArtikliList[CurrentIndex]);
+        }
+
+        void ShowArtikel(IzdelekModel artikel)
+        {
+            textOglasTest.Text = $"{artikel.IzdelekIme}";
         }
 
         private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
